f78f796df16ea4bf8e6ec681bcbca5cc640165ea
icarojuliani25 <00001124534532SP@al.educacao.sp.gov.br>
2024-11-19 16:39:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorALL('.btn-proximo');
+avanca.forEach(button => {
+button.addEventListener("click', function()f
+const atual = document.queryselector(".ativo'); 19-21/ 42
+const proximoPasso = 'passo-'+ this.getAttribute("aata-prox1mo ?
+
+
+atual.classList.remove("ativo');
+document.getElementById(proximoPasso).classList.add('ativo');
+                                                    
